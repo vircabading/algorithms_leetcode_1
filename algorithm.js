fb1f7a6e767c86288a388044893ce8ba2eed8f2b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-20 19:46:52 -0800

    add test cases

--- algorithm.js
+++ algorithm.js
@@ -36,3 +36,4 @@ console.log("Input: nums = [2,7,11,15], target = 9 || output:", twoSum([2,7,11,1
 
 console.log("Input: nums = [3,2,4], target = 6 || output:", twoSum( [3,2,4] , 6));
 
+console.log("Input: nums = [3,3], target = 6 || output:", twoSum( [3,3] , 6));
